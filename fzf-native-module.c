10cd7f8aeaa54c8870bf836afbeae6991ea4d48d
Duc Dang <me@dangduc.com>
2022-06-03 20:18:48 -0700

    Add native module and binding

---
+++ fzf-native-module.c
@@ -0,0 +1,171 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdalign.h>
+#include <string.h>
+#include <ctype.h>
+#include <emacs-module.h>
+#include <pthread.h>
+#include <sys/sysinfo.h>
+#include "fzf.h"
+
+int plugin_is_GPL_compatible;
+
+#define MAX(a, b) ({ __typeof__(a) _a = (a), _b = (b); _a > _b ? _a : _b; })
+
+/** An Emacs string made accessible by copying. */
+struct EmacsStr {
+  emacs_value value; ///< The original string value.
+  size_t len; ///< The length of the string minus the null byte.
+  char b[]; ///< The null-terminated copied string.
+};
+
+struct Worker {
+  pthread_t thread;
+  struct Shared *shared;
+  struct Batch *batch; ///< The initial batch to work on.
+};
+
+static enum JobRetVal {
+  JOB_FINISHED,
+  JOB_FAILED
+} job_finished = JOB_FINISHED, job_failed = JOB_FAILED;
+
+/** Module userdata that gets allocated once at initialization. */
+struct Data {
+  unsigned max_workers;
+  struct Worker *workers;
+};
+
+/** Intrusive linked list of bump allocation blocks. */
+struct Bump {
+  struct Bump *next;
+  size_t index, capacity;
+  char b[];
+};
+
+/**
+ * Allocates the specified number of bytes.
+ *
+ * Returns NULL on failure.
+ */
+static void *bump_alloc(struct Bump **head, size_t len) {
+  if (!*head || (*head)->capacity - (*head)->index < len) {
+    size_t capacity = MAX(*head ? 2 * (*head)->capacity : 1024, len);
+    struct Bump *new_head;
+    if (!(new_head = malloc(sizeof *new_head + capacity)))
+      return NULL;
+    *new_head = (struct Bump) { .next = *head, .index = 0, .capacity = capacity };
+    *head = new_head;
+  }
+
+  void *p = (*head)->b + (*head)->index;
+  (*head)->index += len;
+  return p;
+}
+
+static void bump_free(struct Bump *head) {
+  while (head) {
+    struct Bump *next = head->next;
+    free(head);
+    head = next;
+  }
+}
+
+/**
+ * Copies the Emacs string to make its lifetime that of the allocator.
+ */
+static struct EmacsStr *copy_emacs_string(emacs_env *env, struct Bump **bump, emacs_value value) {
+  ptrdiff_t len;
+  // Determine the size of the string (including null-terminator)
+  env->copy_string_contents(env, value, NULL, &len);
+
+  struct EmacsStr *result;
+  // Note: Since only EmacsStr:s are allocated with bump_alloc we
+  // may use its smaller alignment rather than the scalar maximum.
+  if (!(result = bump_alloc(bump, sizeof *result + len
+                            + alignof(struct EmacsStr) - 1 & ~(alignof(struct EmacsStr) - 1))))
+    return NULL;
+
+  result->value = value;
+  result->len = len - 1;
+  env->copy_string_contents(env, value, result->b, &len);
+  return result;
+}
+
+emacs_value fzf_native_score(emacs_env *env, ptrdiff_t nargs __attribute__ ((__unused__)), emacs_value args[], void *data_ptr) {
+  emacs_value nil = env->intern(env, "nil");
+  emacs_value result = nil;
+
+  // Short-circuit if STR is empty
+  ptrdiff_t str_len;
+  env->copy_string_contents(env, args[0], NULL, &str_len);
+  if (str_len == /* solely null byte */ 1) {
+    result = env->make_integer(env, 0);
+    return result;
+  }
+
+  // Short-circuit if QUERY is empty
+  ptrdiff_t query_len;
+  env->copy_string_contents(env, args[1], NULL, &query_len);
+  if (query_len == /* solely null byte */ 1) {
+    result = env->make_integer(env, 0);
+    return result;
+  }
+
+  struct Bump *bump = NULL;
+
+  struct EmacsStr *str = copy_emacs_string(env, &bump, args[0]);
+  if (!str) goto error;
+
+  struct EmacsStr *query = copy_emacs_string(env, &bump, args[1]);
+  if (!query) goto error;
+
+  fzf_slab_t *slab = fzf_make_default_slab();
+  /* fzf_case_mode enum : CaseSmart = 0, CaseIgnore, CaseRespect
+   * normalize bool     : always set to false because its not implemented yet.
+   *                      This is reserved for future use
+   * pattern char*      : pattern you want to match. e.g. "src | lua !.c$
+   * fuzzy bool         : enable or disable fuzzy matching
+   */
+  fzf_pattern_t *pattern = fzf_parse_pattern(CaseSmart, false, query->b, true);
+
+  /* you can get the score/position for as many items as you want */
+  int score = fzf_get_score(str->b, pattern, slab);
+  fzf_position_t *pos = fzf_get_positions(str->b, pattern, slab);
+
+  fzf_free_positions(pos);
+  fzf_free_pattern(pattern);
+  fzf_free_slab(slab);
+
+  result = env->make_integer(env, score);
+
+ error:
+  return result;
+}
+int emacs_module_init(struct emacs_runtime *rt) {
+  // Verify compatability with Emacs executable loading this module
+  if ((size_t) rt->size < sizeof *rt)
+    return 1;
+  emacs_env *env = rt->get_environment(rt);
+  if ((size_t) env->size < sizeof *env)
+    return 2;
+
+  static struct Data data;
+  data.max_workers = get_nprocs();
+  if (!(data.workers = malloc(data.max_workers * sizeof *data.workers)))
+    return 1;
+
+  env->funcall(env, env->intern(env, "defalias"), 2, (emacs_value[]) {
+      env->intern(env, "fzf-native--score-c"),
+      env->make_function(env, 2, 2, fzf_native_score,
+                         "Filter and sort CANDIDATES that match STRING.\n"
+                         "\n"
+                         "\(fn STRING CANDIDATES)",
+                         &data),
+    });
+
+  env->funcall(env, env->intern(env, "provide"), 1,
+               (emacs_value[]) { env->intern(env, "fzf-native-module") });
+
+  return 0;
+}
---
+++ fzf-native.el
@@ -0,0 +1,51 @@
+;;; fzf-native.el --- Fuzzy completion style  -*- lexical-binding: t; -*-
+
+;; Copyright 2021 Duc Dang
+;; Author: Duc Dang <me@dangduc.com>
+;; Version: 0.1
+;; Package-Requires: ((emacs "27.1"))
+;; Keywords: matching
+;; Homepage: https://github.com/dangduc/fzf-native
+;; SPDX-License-Identifier: GPL-3.0-or-later
+
+;;; Commentary:
+
+;; This is a fuzzy Emacs completion style similar to the built-in
+;; `flex' style, but with a better scoring algorithm.
+
+(require 'cl-lib)
+
+(defgroup fzf-native nil
+  "Fuzzy completion style."
+  :group 'minibuffer
+  :link '(url-link :tag "GitHub" "https://github.com/dangduc/fzf-native"))
+
+(declare-function fzf-native--score-c "fzf-native-module")
+;; If the dynamic module is available: Load it
+
+(defconst fzf-native--dyn-name "fzf-native-module"
+  "Dynamic module name.")
+
+(defconst fzf-native--bin-dir
+  (concat (file-name-directory load-file-name) "bin/")
+  "Pre-built binaries directory path.")
+
+;(require 'fzf-native-module nil nil)
+
+;;;###autoload
+(defun fzf-native-load-dyn ()
+  "Load dynamic module."
+  (interactive)
+  (let* ((dyn-name (cl-case system-type
+                     ((windows-nt ms-dos cygwin) (concat fzf-native--dyn-name ".dll"))
+                     (`darwin (concat fzf-native--dyn-name ".dylib"))
+                     (t (concat fzf-native--dyn-name ".so"))))
+         (dyn-path (concat fzf-native--bin-dir dyn-name)))
+    (module-load dyn-path)
+    (message "[INFO] Successfully load dynamic module, `%s`" dyn-name)))
+
+(defun fzf-native-score (str query)
+  (fzf-native--score-c str query))
+
+(provide 'fzf-native)
+;;; fzf-native.el ends here
