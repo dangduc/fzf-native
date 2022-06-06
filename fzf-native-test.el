811db9c7c02cbec1f1af0f9cb7d54e82e87eed50
Duc Dang <me@dangduc.com>
2022-06-05 23:49:19 -0700

    Add test for slab

--- fzf-native-test.el
+++ fzf-native-test.el
@@ -10,3 +10,11 @@
     (should (= (nth 2 result) 2))
     (should (= (nth 3 result) 4))
     (should (= (nth 4 result) 5))))
+
+(ert-deftest score-with-default-slab-indices-order-test ()
+  (let* ((slab (fzf-native-make-default-slab))
+         (result (fzf-native-score "abcdefghi" "acef" slab)))
+    (should (= (nth 1 result) 0))
+    (should (= (nth 2 result) 2))
+    (should (= (nth 3 result) 4))
+    (should (= (nth 4 result) 5))))
