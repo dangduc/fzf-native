3ab5c90117a908cac1bce2362c5a90acccce89b1
Duc Nguyen <duc@Ducs-MacBook-Pro.local>
2022-07-04 20:15:58 -0700

    Add comment to advice

--- fzf-native.el
+++ fzf-native.el
@@ -124,6 +124,10 @@ FN should be `fzf-native-score'."
                           vconcat (make-vector (string-bytes (char-to-string (aref str i))) i))))
         (cons (car score) (mapcar (lambda (x) (aref idx x)) (cdr score)))))))
 
+;; Work around the lib's lack of support for multibyte chars. Keep
+;; this advice if you want accurate indices for multibyte chars.
+;; Remove this advice if you want better performance or you don't need
+;; accurate indices for multibyte chars.
 (advice-add 'fzf-native-score :around #'fzf-native--fix-score-indices)
 
 (provide 'fzf-native)
