67ae7215a6245af87f2cc05746c5f03c759af56e
James Nguyen <james@jojojames.com>
2024-11-30 15:51:20 -0500

    Change dir-locals to c11

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,3 @@
-((c-mode . ((flycheck-cppcheck-standards . "c99")
-            (flycheck-clang-language-standard . "c99")
-            (flycheck-gcc-language-standard . "c99"))))
+((c-mode . ((flycheck-cppcheck-standards . "c11")
+            (flycheck-clang-language-standard . "c11")
+            (flycheck-gcc-language-standard . "c11"))))
