0601dd74a2b57b17d5f75139fec943a9706523a7
James Nguyen <james@jojojames.com>
2026-05-10 10:17:14 -0400

    Wipe text properties before adding them

--- fzf-native.el
+++ fzf-native.el
@@ -21,6 +21,7 @@
   :link '(url-link :tag "GitHub" "https://github.com/dangduc/fzf-native"))
 
 (declare-function fzf-native-score-all "fzf-native-module" (collection query &optional slab))
+(declare-function fzf-native-highlight-all "fzf-native-module" (collection query))
 (declare-function fzf-native-score "fzf-native-module" (str query &optional slab))
 (declare-function fzf-native-make-default-slab "fzf-native-module" ())
 (declare-function fzf-native-make-slab "fzf-native-module" (size16 size32))
